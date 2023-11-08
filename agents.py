cf6ab0fbaf0f8db910140e907fab22958c42ad68
Imraan <mitha019@umn.edu>
2023-11-07 21:14:31 -0600

    Added saving functionality for models, plots and loss arrays, moved hyperparameters into dictionary

--- agents.py
+++ agents.py
@@ -10,44 +10,42 @@ from utils import *
 # agent consists of four networks, critic/actor and main/target
 #TODO: implement loading networks
 class DDPGagent:
-    def __init__(self, env, device, hidden_size=512, actor_learning_rate=1e-4, critic_learning_rate=1e-4, gamma=0.5, tau=1e-1, g_noise_std = 0.1*np.pi, replay_buffer_size=50000):
+    def __init__(self, env, device, hypers):
         # Params
+        self.hypers = hypers
         self.num_actions = len(env.configuration)
         self.num_states = len(env.state)
-        self.gamma = gamma
-        self.tau = tau
-        self.gaussian_noise_std = g_noise_std
+        self.gamma = hypers["gamma"]
+        self.tau = hypers["tau"]
+        self.action_noise = hypers["action_noise"]
+        self.gaussian_noise_std = hypers["g_noise_std"]
         self.ou_noise = OUNoise(self.num_actions)
         self.device = device
 
         # Networks
+        hidden_size = hypers["hidden_units"]
         self.actor = Actor(self.num_states, hidden_size, self.num_actions).to(self.device)
         self.actor_target = Actor(self.num_states, hidden_size, self.num_actions).to(self.device)
         self.critic = Critic(self.num_states + self.num_actions, hidden_size, 1).to(self.device)
         self.critic_target = Critic(self.num_states + self.num_actions, hidden_size, 1).to(self.device)
 
         # copy target networks state dicts
-        # this seems like a stupid way to do this, surely theres a better way
-        for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
-            target_param.data.copy_(param.data)
-
-        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
-            target_param.data.copy_(param.data)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.actor_target.load_state_dict(self.actor.state_dict())
         
         # Training tools
-        self.replay_buffer = ReplayBuffer(replay_buffer_size)        
+        self.replay_buffer = ReplayBuffer(hypers["replay_buffer_size"])        
         self.critic_criterion  = nn.MSELoss()
-        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=actor_learning_rate)
-        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=critic_learning_rate)
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=hypers["policy_lr"])
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=hypers["critic_lr"])
 
         # Loss histories
         self.policy_loss_history = [] # track policy loss, can delete later
         self.critic_loss_history = [] # track critic loss, can delete later
 
 
-    def get_action(self, state, step, add_noise = None):
+    def get_action(self, state, step):
         #TODO: does actor need to be no_grad here?
-
         self.actor.eval()
 
         state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
@@ -58,10 +56,10 @@ class DDPGagent:
 
         action_no_noise = np.copy(action) # just for a test I am running delete this later
 
-        if add_noise == 'G':
+        if self.action_noise == 'G':
             noise = np.random.normal(0, self.gaussian_noise_std, action.shape)
             action = action + noise 
-        elif add_noise == "OU":
+        elif self.action_noise == "OU": #not well implemented, sigmas are way too big
             action = self.ou_noise.add_noise(action, step)
 
         action = np.clip(action, -0.1, 0.1) #TODO: make the action bounds class variables for agent, used here, in actor network definition, and in plotting in main to viz actions
@@ -89,19 +87,10 @@ class DDPGagent:
         # critic_loss = self.critic_criterion(current_Q, target_Q)
         critic_loss = nn.functional.mse_loss(current_Q, target_Q)
 
-        
         # optimize critic
         self.critic_optimizer.zero_grad()
         critic_loss.backward() 
         self.critic_optimizer.step()
-        
-        # #####old#####
-        # Qvals = self.critic.forward(states, actions)
-        # next_actions = self.actor_target.forward(next_states)
-        # # next_Q = self.critic_target.forward(next_states, next_actions.detach()) # why detach here?
-        # next_Q = self.critic_target.forward(next_states, next_actions)
-        # Qprime = rewards + self.gamma * next_Q # yi in paper
-        # critic_loss = self.critic_criterion(Qvals, Qprime)
 
         # Actor loss
         policy_loss = -self.critic(states, self.actor(states)).mean()
@@ -126,8 +115,7 @@ class DDPGagent:
     '''
     saves all 4 models under ./models/ directory in increminting subdirectories
     '''
-    def save_models(self):
-        models_path = 'models'
+    def save(self, models_path, rewards, avg_rewards, fig):
         zero_fill = 4
 
         existing_subdirs = [d for d in os.listdir(models_path) if os.path.isdir(os.path.join(models_path, d))]
@@ -147,8 +135,20 @@ class DDPGagent:
         torch.save(self.critic_target.state_dict(), os.path.join(next_dir_path, 'critic_target.pth'))
 
         # save parameters to a txt file
-        #TODO: finish implementing, I will change hyperparams to be passed as a dict so we can have all of them here and keep the code clean but I first want to make sure I didnt mess anything up so far and push the changes
         parameter_file = os.path.join(next_dir_path, "parameters.txt")
         with open(parameter_file, 'w') as f:
-            f.write()
+            for key, value in self.hypers.items():
+                f.write(f"{key}:\t{value}\n")
+        
+        # save loss arrays
+        np.save(os.path.join(next_dir_path, "policy_loss.npy"), np.array(self.policy_loss_history))
+        np.save(os.path.join(next_dir_path, "critic_loss.npy"), np.array(self.critic_loss_history))
+        np.save(os.path.join(next_dir_path, "rewards.npy"), np.array(rewards))
+        np.save(os.path.join(next_dir_path, "avg_rewards.npy"), np.array(avg_rewards))
+
+        # save plots png
+        fig.savefig(os.path.join(next_dir_path, "plots.png"))
+
+        #TODO: should save loss and reward history as maybe .npy too so we can load and continue those plots if we want
+
 
--- main.py
+++ main.py
@@ -5,32 +5,34 @@ from utils import *
 from manipulator_environment import *
 import time
 
-def train_loop():
+def train_loop(models_path=None):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # hyperparamers
-    num_epochs = 200
-    batch_size = 1024
-    policy_lr=5e-2
-    critic_lr=1e-4
-    gamma=0.9
-    tau=0.005
-    noise_std = 0.005*np.pi
-    replay_buffer_size=int(1e6)
-    hidden_units = 512
-    num_steps = 1000 # number of steps per episode
+    hypers = {"num_epochs": 500,
+                    "batch_size": 1024,
+                    "policy_lr": 1e-3,
+                    "critic_lr": 1e-4,
+                    "gamma": 0.7,
+                    "tau": 0.005,
+                    "action_noise": None,
+                    "g_noise_std": 0.005*np.pi,
+                    "replay_buffer_size": int(1e6),
+                    "hidden_units": 512,
+                    "num_steps": 1000
+                    }
 
     # inits
     env = Planar_Environment()
-    agent = DDPGagent(env, device, hidden_size=hidden_units, actor_learning_rate=policy_lr, critic_learning_rate=critic_lr, gamma=gamma, tau=tau, noise_std=noise_std, replay_buffer_size=replay_buffer_size)
+    agent = DDPGagent(env, device, hypers)
     rewards = []
     avg_rewards = []
 
     start_time = time.time()
     print(f'Beginning training on {device}')
-    print(f'\n{num_epochs} epochs, {num_steps} steps, {batch_size} batch size, lr_p={policy_lr}, lr_c=c{critic_lr}, gamma={gamma}, tau={tau}, {hidden_units} hidden')
+    print(f'\n{hypers["num_epochs"]} epochs, {hypers["num_steps"]} steps, {hypers["batch_size"]} batch size, lr_p={hypers["policy_lr"]}, lr_c={hypers["critic_lr"]}, gamma={hypers["gamma"]}, tau={hypers["tau"]}, {hypers["hidden_units"]} hidden units')
 
-    for episode in range(num_epochs):
+    for episode in range(hypers["num_epochs"]):
         epoch_start_time = time.time()
         state = env.reset()
         # env.viz_arm()
@@ -39,11 +41,8 @@ def train_loop():
         action_history = []
         reward_history = []
 
-        # agent.policy_loss_history = []
-        # agent.critic_loss_history = []
-
-        for step in range(num_steps):
-            action, action_no_noise = agent.get_action(state, step, add_noise="G")
+        for step in range(hypers["num_steps"]):
+            action, action_no_noise = agent.get_action(state, step)
             new_state, reward, done = env.step(action)
             # env.viz_arm()
 
@@ -52,19 +51,19 @@ def train_loop():
 
             agent.replay_buffer.push(state, action, reward, new_state, done)
             
-            if len(agent.replay_buffer) > batch_size:
-                agent.update(batch_size)
+            if len(agent.replay_buffer) > hypers["batch_size"]:
+                agent.update(hypers["batch_size"])
                     
             state = new_state
 
             episode_reward += reward
 
             if done:
-                print(f'Completed in episode {episode} by step {step}, with episode average reward: {round(episode_reward / num_steps, 3)}')
+                print(f'Completion in episode {episode} by step {step}, with episode average reward: {round(episode_reward / hypers["num_steps"], 3)}')
                 break
 
 
-        rewards.append(episode_reward / num_steps)
+        rewards.append(episode_reward / hypers["num_steps"])
         avg_rewards.append(np.mean(rewards[-10:]))
 
         print(f'Episode {episode}, average reward: {round(rewards[-1], 3)}, in {round(time.time()-epoch_start_time, 3)} s')
@@ -73,18 +72,13 @@ def train_loop():
         # action_history = np.array(action_history)
         # reward_history = np.array(reward_history)
         # fig, axs = plt.subplots(3, 1, figsize=(20,9))
-
         # axs[0].plot(action_history[:, 0], label='action[0]')
         # axs[0].plot(action_history[:, 1], label='action[1]')
         # axs[0].plot(0.5*np.ones_like(action_history[:, 0]), color='red', linewidth = 0.5)
         # axs[0].plot(-0.5*np.ones_like(action_history[:, 0]), color='red', linewidth = 0.5)
-        
         # axs[1].plot(reward_history)
-
         # axs[2].plot(rewards)
-        
         # axs[0].legend()
-
         # axs[0].set_title("no noise actions")
         # axs[1].set_title("reward")
         # axs[2].set_title("episode avg rewards")
@@ -94,7 +88,8 @@ def train_loop():
         # plt.show()  
 
 
-    print(f'\nTotal time for {num_epochs} epochs: {round(time.time()-start_time, 3)}')
+    print(f'\nTotal time for {hypers["num_epochs"]} epochs: {round(time.time()-start_time, 3)}')
+
     # code to visualize the actor/critic losses throughout the episode as well as the episodes reward
     fig, axs = plt.subplots(3, 1, figsize=(10, 9))
     axs[0].plot(agent.policy_loss_history, label="policy loss")
@@ -110,20 +105,14 @@ def train_loop():
     axs[2].grid(True)
     axs[2].legend()
     axs[2].set_title("episode avg rewards")
-    fig.canvas.manager.set_window_title(f'{num_epochs} epochs, {num_steps} steps, {batch_size} batch size, lr_p={policy_lr}, lr_c=c{critic_lr}, gamma={gamma}, tau={tau}, {hidden_units} hidden')
-
-    print(f'\n{num_epochs} epochs, {num_steps} steps, {batch_size} batch size, lr_p={policy_lr}, lr_c=c{critic_lr}, gamma={gamma}, tau={tau}, {hidden_units} hidden')
+    fig.suptitle(f'{hypers["num_epochs"]} epochs, {hypers["num_steps"]} steps, {hypers["batch_size"]} batch size, lr_p={hypers["policy_lr"]}, lr_c={hypers["critic_lr"]}, gamma={hypers["gamma"]}, tau={hypers["tau"]}, {hypers["hidden_units"]} hidden units')
+    
+    if models_path is not None:
+        agent.save(models_path, rewards, avg_rewards, fig)
+    else:
+        print("Cant save models, no path provided")
 
     plt.show()
 
-
-    # standard plotting of epoch avg rewards and 10 epoch sliding average
-    # plt.plot(rewards, label="Episode avg reward")
-    # plt.plot(avg_rewards, label="10 Episode sliding average")
-    # plt.legend()
-    # plt.xlabel('Episode')
-    # plt.ylabel('Average Reward')
-    # plt.show()
-
 if __name__ == "__main__":
-    train_loop()
+    train_loop("models")
